45b611c443cd2135e62a72f2ebe401b4338f9b16
M <malekuk9@gmail.com>
2024-07-29 20:39:29 +0100

    no .env

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:choosey/welcome.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
+// import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 Future<void> main() async {
-  await dotenv.load();
+  // await dotenv.load();
   runApp(const MyApp());
 }
 
