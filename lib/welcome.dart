564ade939b2353840272cf073fc6dc77c4644ba7
M <malekuk9@gmail.com>
2024-08-06 17:53:54 +0100

    -scope

--- lib/welcome.dart
+++ lib/welcome.dart
@@ -8,7 +8,7 @@ import 'package:google_sign_in/google_sign_in.dart';
 
 const List<String> scopes = <String>[
   'email',
-  'https://www.googleapis.com/auth/contacts.readonly',
+  // 'https://www.googleapis.com/auth/contacts.readonly',
   'https://www.googleapis.com/auth/userinfo.profile',
   'https://www.googleapis.com/auth/userinfo.email',
   'openid'
